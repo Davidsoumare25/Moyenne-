856582684c804a51c02a185d98458f87fefbe0f9
Davidsoumare25 <soumaredaouda784@gmail.com>
2026-02-06 16:20:02 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,36 @@
-const CACHE_NAME = 'moyenne-sn-v2';
+const CACHE_NAME = 'moyenne-sn-v2'; // Change v1 en v2 ici
 const ASSETS = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-// Installation : Mise en cache des fichiers
+// Installation et mise en cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  // Force la nouvelle version à s'installer immédiatement
+  self.skipWaiting(); 
 });
 
-// Stratégie : Cache first, puis réseau
+// Nettoyage des anciens caches pour les utilisateurs
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
-
