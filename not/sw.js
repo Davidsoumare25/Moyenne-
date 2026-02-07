aa1391eb91ac20404778d6385584c4ffea81471f
Davidsoumare25 <soumaredaouda784@gmail.com>
2026-02-07 21:30:47 +0000

    Delete not/sw.js

--- not/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('push', function(event) {
-    const options = {
-        body: event.data.text(),
-        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
-        vibrate: [200, 100, 200]
-    };
-    event.waitUntil(self.registration.showNotification('Calculateur Sénégal Pro', options));
-});
-
