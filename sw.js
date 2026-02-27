276837fa97a3d1d6dd8d6a2d0cbf7278ed1e636f
Davidsoumare25 <soumaredaouda784@gmail.com>
2026-02-27 17:41:46 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Force le nouveau service worker à s'activer
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim()); // Prend le contrôle des pages immédiatement
+});
 
